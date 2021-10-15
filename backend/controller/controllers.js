29da8a7adf62a03d308b13d006a9b47537317e7a
Otto <justigo86@gmail.com>
2021-10-15 09:55:25 -0400

    completed backend for now - moving to update frontend

--- backend/controller/controllers.js
+++ backend/controller/controllers.js
@@ -1,12 +1,26 @@
 const product = require('../models/product');
 
-const getProducts = async (re, res) => {
+const getProducts = async (req, res) => {
   try {
     const products = await product.find({});    //.find to query key/values to return filtered array
     res.json(products);
   } catch (err) {
     console.log(err);
+    res.status(500).json('server error')
   }
 }
 
-module.exports = getProducts;
+const getProduct = async (req, res) => {
+  try {
+    const products = await product.findById(req.params.id);    //.find to query key/values to return filtered array
+    res.json(products);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json('server error')
+  }
+}
+
+module.exports = {
+  getProducts,
+  getProduct
+};
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {   //get all products from database
+const { getProduct, getProducts } = require('../controller/controllers');
 
-})
+router.get('/', getProducts);   //get all products from database
 
-router.get('/:id', (req, res) => {   //get specific product from database
-
-})
+router.get('/:id', getProduct);   //get specific product from database
 
 module.exports = router;
