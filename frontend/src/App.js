464f028ecdd248d5fe2bc81a3377ab5569387ff0
Otto <justigo86@gmail.com>
2021-10-19 20:22:10 -0400

    continue frontend - adding routes for pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
 
 const App = () => {
   return (
-    <div className="App">
-      Testing
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/product/:id' component={Product} />
+            <Route path='/cart' component={Cart} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Product.js
@@ -0,0 +1,10 @@
+
+const Product = () => {
+  return (
+    <div>
+      Product      
+    </div>
+  )
+}
+
+export default Product
